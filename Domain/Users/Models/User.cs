ed3e8f40e9ada322f9194b7f4362a07c3ba211b3
Daniel Stott <wdstott@hotmail.co.uk>
2021-04-21 00:10:03 +0100

    Added mediator and user handler

---
+++ Domain/Users/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Domain.Users.Models
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public static User Create(string email, string password, string firstName, string lastName) 
+            => new()
+            {
+                Id = Guid.NewGuid(),
+                Email = email,
+                Password = password,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+    }
+}
